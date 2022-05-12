ca8d519485dd0b161f3ab5c7e123027471d28a6c
Wilder Samuel Taborda Cerón <est.wilder.taborda@unimilitar.edu.co>
2022-05-12 17:09:49 -0500

    removing error calculation

--- calibrateCamera.py
+++ calibrateCamera.py
@@ -9,40 +9,15 @@ import time
 
 cameraMatrix, dist, rvecs, tvecs,objpoints, imgpoints = getCoefficients()
 
-fps = 10 
-
 cap = cv.VideoCapture(0)
-# Read the image of the video capture constantly
+
 while True:
-    startTime = time.time()
-    # Get the image
     _,img = cap.read()
-
-    # Flip the image
     img = cv.flip(img,1)
-
-    # Get the dimensions of the image
     h,  w = img.shape[:2]
-
-    # Calibrate the image
     img = calibrate(img, cameraMatrix, dist,w,h)
-
-    # Reprojection Error
-    mean_error = 0
-    for i in range(len(objpoints)):
-        imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
-        error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
-        mean_error += error
-    error =  "total error: {}".format(mean_error/len(objpoints))
-
-    # Display the image
     cv.imshow("img",img)
-
-    # Code to get outta loop
-
-    k = cv.waitKey(10)
-    operationPerSecond = 1/(time.time() - startTime)
-    print(operationPerSecond)
+    k = cv.waitKey(1)
     if k == 27:
         break
 
