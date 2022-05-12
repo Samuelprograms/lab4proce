850d1676ca30b1350a4b8f46d63d57e209b0a6e7
Wilder Samuel Taborda Cerón <est.wilder.taborda@unimilitar.edu.co>
2022-05-12 16:20:56 -0500

    reusing code

--- calibrateCamera.py
+++ calibrateCamera.py
@@ -1,18 +1,20 @@
 import sys
 sys.path.append('C:/Users/Wilder Taborda/Desktop/universidad/lab4proce/camera')
-from cameraHandler import getCoefficients
+from cameraHandler import getCoefficients,calibrate
 import numpy as np
 import cv2 as cv
 import glob
 import os.path
+import time
 
 cameraMatrix, dist, rvecs, tvecs,objpoints, imgpoints = getCoefficients()
 
-cap = cv.VideoCapture(0)
+fps = 10 
 
+cap = cv.VideoCapture(0)
 # Read the image of the video capture constantly
 while True:
-
+    startTime = time.time()
     # Get the image
     _,img = cap.read()
 
@@ -22,39 +24,25 @@ while True:
     # Get the dimensions of the image
     h,  w = img.shape[:2]
 
-    # Get the camera matrix and distortion coefficients
-    newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
-
-    # Undistort
-    dst = cv.undistort(img, cameraMatrix, dist, None, newCameraMatrix)
-
-    # crop the image
-    x, y, w, h = roi
-    dst = dst[y:y+h, x:x+w]
-
-    # Undistort with Remapping
-    mapx, mapy = cv.initUndistortRectifyMap(cameraMatrix, dist, None, newCameraMatrix, (w,h), 5)
-    dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
-
-    # crop the image
-    x, y, w, h = roi
-    dst = dst[y:y+h, x:x+w]
+    # Calibrate the image
+    img = calibrate(img, cameraMatrix, dist,w,h)
 
     # Reprojection Error
     mean_error = 0
-
     for i in range(len(objpoints)):
         imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
         error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
         mean_error += error
-
     error =  "total error: {}".format(mean_error/len(objpoints))
 
     # Display the image
-    cv.imshow("img",dst)
+    cv.imshow("img",img)
 
     # Code to get outta loop
-    k = cv.waitKey(1)
+
+    k = cv.waitKey(10)
+    operationPerSecond = 1/(time.time() - startTime)
+    print(operationPerSecond)
     if k == 27:
         break
 
